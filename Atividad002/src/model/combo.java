66773454b61bd365fa570640ed62108022aee863
JuanDiegoMH <juandiegomunguiahuaya@gmail.com>
2025-09-29 11:50:20 -0500

    combo

---
+++ Atividad002/src/model/combo.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Combo {
+    private String base;
+    private String bebida;
+    private String acompanamiento;
+    private String extra;
+
+    public Combo(String base) {
+        this.base = base;
+    }
+
+    public void setBebida(String bebida) {
+        this.bebida = bebida;
+    }
+
+    public void setAcompanamiento(String acompanamiento) {
+        this.acompanamiento = acompanamiento;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+
+    @Override
+    public String toString() {
+        return "Combo [Base=" + base +
+                ", Bebida=" + bebida +
+                ", Acompañamiento=" + acompanamiento +
+                ", Extra=" + extra + "]";
+    }
+}
