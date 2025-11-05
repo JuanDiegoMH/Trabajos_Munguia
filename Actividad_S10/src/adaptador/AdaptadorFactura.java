0ad2ee0052f5ebaa26f29a387a7130e86532dffd
JuanDiegoMH <juandiegomunguiahuaya@gmail.com>
2025-11-05 10:53:15 -0500

    Create AdaptadorFactura.java

---
+++ Actividad_S10/src/adaptador/AdaptadorFactura.java
@@ -0,0 +1,22 @@
+package adaptador;
+
+import legado.SistemaFacturacionAntiguo;
+import modelo.Pedido;
+import servicio.ServicioFactura;
+
+public class AdaptadorFactura implements ServicioFactura {
+    private SistemaFacturacionAntiguo sistemaAntiguo;
+
+    public AdaptadorFactura(SistemaFacturacionAntiguo sistemaAntiguo) {
+        this.sistemaAntiguo = sistemaAntiguo;
+    }
+
+    @Override
+    public void generarFactura(Pedido pedido) {
+        sistemaAntiguo.crearFacturaAntigua(
+            pedido.getCliente(),
+            pedido.getProducto().getNombre(),
+            pedido.getTotal()
+        );
+    }
+}
